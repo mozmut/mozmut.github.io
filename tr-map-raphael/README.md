2d7564d051a178d6bee9dbc4a9708edee6e08ab0
Mert Özmut <is95536@gm.isbank.com.tr>
2022-03-07 18:44:03 +0300

    add tr-map-raphael

---
+++ tr-map-raphael/README.md
@@ -0,0 +1,15 @@
+# tr-map-raphael
+
+This repository contains svg paths for 81 provinces in Turkey and those svg paths were used to create an interactive map by using raphael.js.
+
+## Demo
+[View demo](http://mozmut.github.io/tr-map-raphael/demo/)
+
+## License
+
+The map coordinates and demo code are both public domain.
+
+## Inspired
+
+https://github.com/SelenGora/TurkeyMap <br />
+https://github.com/robflaherty/us-map-raphael
---
+++ tr-map-raphael/js/draw.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+	var R = Raphael("izmir", 1024, 800);
+	R.rect(0, 0, 1024, 800, 0).attr({
+        stroke: "none",
+        fill: "0-#9bb7cb-#adc8da"
+    });
+	var attr = {
+		"fill": "#d3d3d3",
+		"stroke": "#000",
+		"stroke-opacity": "1",
+		"stroke-linejoin": "round",
+		"stroke-miterlimit": "4",
+		"stroke-width": "1.0",
+		"stroke-dasharray": "none"
+	};
+	
+	mPaths = izmirMap.paths;
+	mRaphael = {};
+	
+	for (var province in mPaths) {
+		mRaphael[province] = R.path(mPaths[province]).attr(attr);
+	}
+	
+    for (var province in mRaphael) {
+    	mRaphael[province].color = Raphael.getColor();
+        
+        (function (pr, province) {
+          pr[0].style.cursor = "pointer";
+          pr[0].onmouseover = function () {
+        	  pr.animate({fill: pr.color}, 500);
+        	  pr.toFront();
+          };
+          pr[0].onmouseout = function () {
+        	  pr.animate({fill: "#d3d3d3"}, 500);
+        	  pr.toFront();
+          };
+                     
+        })(mRaphael[province], province);
+      }
+};
